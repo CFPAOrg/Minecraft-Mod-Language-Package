f7d76a45acc1b7fd0695b0a85c9f9ec8bc3cd34d
TartaricAcid <baka943@qq.com>
2017-12-16 14:18:31 +0800

    Update run.sh - 更新了批量化处理脚本，现在能够自动剔除已有冗余重复的汉化 - 处理脚本目前还存在部分问题，识别过程中部分相同字符串还是无法正确匹配 - 更新了1.12.2的仓库，剔除了部分冗余语言文件

--- run.sh
+++ run.sh
@@ -82,7 +82,6 @@ for filename_mods in `ls`
 		for filename_assets in `ls`
 			do
 				if [ "$filename_mods" == "$filename_assets" ]; then
-　　			echo "汉化替换"
 					mv "${PATH_MODS}/${filename_mods}/lang/zh_CN-merged.lang" "${PATH_MAIN}/en_US.lang"
 					mv "${PATH_ASSETS}/${filename_assets}/lang/zh_CN.lang" "${PATH_MAIN}/zh_CN.lang"
 					cd ${PATH_MAIN}
@@ -110,6 +109,55 @@ for filename_assets in `ls`
 			mv "${PATH_ASSETS}/${filename_assets}/lang/zh_CN.lang" "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang"
 			cd $PATH_ASSETS
 		fi
+done
+
+# 最后，删掉assets-tmp文件夹
+cd ${PATH_MAIN}
+rm -rf ./assets-tmp
+
+# 我想应该没有比我还傻的了，懒得改动以前的代码，重新再来一遍吧
+# 新建assets-tmp文件夹，放置临时从mod包中解压的语言文件
+mkdir assets-tmp
+
+# 这一块有一个奇怪的bug，如果在主目录下操作解压，整个解压会全部出错
+# 考虑到1.12的特殊性，有的作者用小写，有的用大写
+cd mods
+for filename in `ls *.jar`
+	do
+		unzip $filename assets/*/lang/zh_CN.lang -d ${PATH_MAIN}/assets-tmp/
+		unzip $filename assets/*/lang/zh_cn.lang -d ${PATH_MAIN}/assets-tmp/
+	done
+echo "完成语言文件解压"
+
+
+# 我管你大写还是小写，统统给我小写
+cd $PATH_MODS
+for filename_mods in `ls`
+	do
+		if [ -f "${PATH_MODS}/${filename_mods}/lang/zh_CN.lang" ]; then
+			echo "${filename_mods}模组的中文文件改写成小写"
+			mv "${PATH_MODS}/${filename_mods}/lang/zh_CN.lang" "${PATH_MODS}/${filename_mods}/lang/zh_cn.lang"
+			cd $PATH_MODS
+		fi
+done
+
+# 遍历assets-tmp文件夹中所有子文件夹，对比assets文件夹中文件
+# 对比中文文件，只记录增加的部分
+cd $PATH_MODS
+for filename_mods in `ls`
+ do
+	 cd $PATH_ASSETS
+	 for filename_assets in `ls`
+		 do
+			 # 很奇怪的问题，有些mod会莫名其妙存在minecraft文件夹
+			 if [ "$filename_mods" == "$filename_assets" ] && [ "$filename_assets" != "minecraft" ]; then
+				 diff -u -B "${PATH_MODS}/${filename_mods}/lang/zh_cn.lang" "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang" | grep ^\+ > "${PATH_ASSETS}/${filename_assets}/lang/diff.lang"
+				 sed -i 's/^[+]*//g' "${PATH_ASSETS}/${filename_assets}/lang/diff.lang"
+				 rm "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang"
+				 mv "${PATH_ASSETS}/${filename_assets}/lang/diff.lang" "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang"
+				 break
+			 fi
+	 done
 done
 
 # 最后，删掉assets-tmp文件夹
