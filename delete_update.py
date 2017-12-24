00524ef3d05523ca538945fa01f4ba3e76f9ccfe
TartaricAcid <baka943@qq.com>
2017-12-24 13:13:39 +0800

    Fix update script error

---
+++ delete_update.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# @Author TartaricAcid
+# @Title 批量本地化更新工具（增量更新版本）
+######################################
+
+print("add_update Script Loading")
+
+import operator
+
+# 读取文件
+asset_old = open("zh_cn_old.lang", 'r', encoding='UTF-8')
+asset_new = open("zh_cn_new.lang", 'r', encoding='UTF-8')
+asset_zh= open("zh_cn.lang", 'w', encoding='UTF-8')
+
+# 放置映射表
+old_dict = dict()
+new_dict = dict()
+
+# 放置key，用以for循环查找
+new_key = list()
+
+for entry in asset_old.readlines():
+    if entry != None and entry[0] != '#' and entry[0] != '/' and '=' in entry:
+        entry_list = entry.split('=', 1)        # 依据等号切分语言文件条目
+        old_dict[entry_list[0]] = entry_list[1]
+
+for entry in asset_new.readlines():
+    if entry != None and entry[0] != '#' and entry[0] != '/' and '=' in entry:
+        entry_list = entry.split('=', 1)        # 依据等号切分语言文件条目
+        new_key.append(entry_list[0])            # 空key文件为之后for循环查找提供参考
+        new_dict[entry_list[0]] = entry_list[1]
+
+for entry in new_key:
+    try:
+        if operator.eq(new_dict[entry], old_dict[entry]):
+            pass
+        else:
+            asset_zh.writelines(entry + '=' + new_dict[entry])
+    except:     # 接收异常，对英文key和英文value进行拼接
+        asset_zh.writelines(entry + '=' + new_dict[entry])
+
+asset_old.close()
+asset_new.close()
+asset_zh.close()
+
+print("add_update Script Stop Load")
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#!/usr/bin/python3
+#! /usr/bin/python3
 
 # 记录操作的主目录，方便后面操作
 PATH_MAIN=`pwd`
@@ -56,7 +56,7 @@ for filename_mods in `ls`
 			cp "${PATH_MODS}/${filename_mods}/lang/en_us.lang" "${PATH_MAIN}/en_us.lang"
 			cp "${PATH_MODS}/${filename_mods}/lang/zh_cn.lang" "${PATH_MAIN}/zh_cn.lang"
 			cd ${PATH_MAIN}
-			python3 add_update.py
+			python3 all_update.py
 			mv "${PATH_MAIN}/zh_cn_out.lang" "${PATH_MODS}/${filename_mods}/lang/zh_cn_out.lang"
 			rm ${PATH_MAIN}/en_us.lang
 			rm ${PATH_MAIN}/zh_cn.lang
@@ -90,6 +90,52 @@ for filename_mods in `ls`
 			mv "${PATH_MODS}/${filename_mods}/lang/zh_cn_out.lang" "${PATH_ASSETS}/${filename_mods}/lang/zh_cn.lang"
 			cd $PATH_MODS
 		fi
+done
+
+# 最后，删掉assets-tmp文件夹
+cd ${PATH_MAIN}
+rm -rf ./assets-tmp
+mkdir assets-tmp
+
+# 二次解压中文语言文件，进行冗余剔除
+cd mods
+for filename in `ls`
+	do
+		unzip -o $filename assets/*/lang/zh_CN.lang -d ${PATH_MAIN}/assets-tmp/
+		unzip -o $filename assets/*/lang/zh_cn.lang -d ${PATH_MAIN}/assets-tmp/
+	done
+echo "完成中文语言文件解压"
+
+# 我管你大写还是小写，统统给我小写
+cd $PATH_MODS
+for filename_mods in `ls`
+	do
+		if [ -f "${PATH_MODS}/${filename_mods}/lang/zh_CN.lang" ]; then
+			echo "${filename_mods}模组的中文文件改写成小写"
+			mv "${PATH_MODS}/${filename_mods}/lang/zh_CN.lang" "${PATH_MODS}/${filename_mods}/lang/zh_cn.lang"
+			cd $PATH_MODS
+		fi
+done
+
+# 遍历assets-tmp文件夹中所有子文件夹，对比assets文件夹中文件
+# 如果有相同的，就对比更新语言文件
+cd $PATH_MODS
+for filename_mods in `ls`
+	do
+		cd $PATH_ASSETS
+		for filename_assets in `ls`
+			do
+				if [ "$filename_mods" == "$filename_assets" ]; then
+					mv "${PATH_MODS}/${filename_mods}/lang/zh_cn.lang" "${PATH_MAIN}/zh_cn_old.lang"
+					mv "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang" "${PATH_MAIN}/zh_cn_new.lang"
+					cd ${PATH_MAIN}
+					python3 delete_update.py
+					mv "${PATH_MAIN}/zh_cn.lang" "${PATH_ASSETS}/${filename_assets}/lang/zh_cn.lang"
+					rm ${PATH_MAIN}/zh_cn_old.lang
+					rm ${PATH_MAIN}/zh_cn_new.lang
+					break
+				fi
+		done
 done
 
 # 最后，删掉assets-tmp文件夹
