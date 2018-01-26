ffa5b2ceb5fd51b019951644a105f0130d0c3138
TartaricAcid <baka943@qq.com>
2018-01-26 13:37:35 +0800

    增加删除名单功能，现在可以强制删除 project/assets 下面指定的文件

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+import yaml
 
 # 清除先前的零时文件
 os.system('rm -rf /tmp/mods')
@@ -29,3 +30,13 @@ import src.handle.handle
 # 再次清除先前的零时文件
 os.system('rm -rf /tmp/mods')
 os.system('rm -rf /tmp/modpacks')
+
+# 开始检索 del list 删除
+# 读取配置文件
+with open('config.yml', 'r') as f:
+    config = yaml.load(f)
+    DEL_LIST = config['del_list']
+
+# 开始强制删除对应文件夹
+for i in DEL_LIST:
+    os.system('rm -rf ./project/assets/' + i)
