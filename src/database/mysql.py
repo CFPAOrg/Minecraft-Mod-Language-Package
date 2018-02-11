bb3f2f8a948c268bb2442627c73b06dbb0836d86
TartaricAcid <baka943@qq.com>
2018-02-12 01:51:39 +0800

    数据库导入优化，从 9s 提升至 2.8s； 高频词分析

--- src/database/mysql.py
+++ src/database/mysql.py
@@ -8,7 +8,7 @@ def lang_to_dict(file_path):
     lang_dict = {}
     with open(file_path, 'r', errors='ignore') as f:
         for line in f.readlines():
-            if line != None and line[0] != '#' and line[0] != '/' and '=' in line:
+            if line is not None and line[0] != '#' and line[0] != '/' and '=' in line:
                 # 剔除行末尾 \n
                 line = line.rstrip('\n')
                 line_list = line.split('=', 1)
@@ -22,35 +22,42 @@ db = pymysql.connect(host='localhost', user='',
 # 创建一个游标对象 cursor
 cursor = db.cursor()
 # 如果表存在则删除
-cursor.execute("DROP TABLE IF EXISTS LANG")
+cursor.execute('DROP TABLE IF EXISTS LANG')
 
 # 使用预处理语句创建表
 # 限定只装入 16 字符汉字，256 字符英文
 cursor.execute("""CREATE TABLE LANG (
-         modid varchar(64),
-         en_us varchar(256),
-         zh_cn varchar(16))
-         CHARSET=utf8mb4""")
+         modid VARCHAR(64),
+         en_us VARCHAR(256),
+         zh_cn VARCHAR(16))
+         CHARSET='utf8mb4'""")
 
+# 导入的数据 list
+data_list = []
 # 开始遍历文件了
-for modid in os.listdir('project/assets'):
+for modid in os.listdir('../../project/assets'):
     # 先判定 en_us.lang 是否存在
-    if not os.path.exists('project/assets/{}/lang/en_us.lang'.format(modid)):
+    if not os.path.exists('../../project/assets/{}/lang/en_us.lang'.format(modid)):
         continue
+
     # 而后开始转换为 dict
     en_us_dict = lang_to_dict(
-        'project/assets/{}/lang/en_us.lang'.format(modid))
+        '../../project/assets/{}/lang/en_us.lang'.format(modid))
     zh_cn_dict = lang_to_dict(
-        'project/assets/{}/lang/zh_cn.lang'.format(modid))
+        '../../project/assets/{}/lang/zh_cn.lang'.format(modid))
 
     # 开始写入数据库
     for k in zh_cn_dict.keys():
-        if k in en_us_dict:
-            if len(en_us_dict[k]) <= 256 and len(zh_cn_dict[k]) <= 16 and '"' not in en_us_dict[k] and '"' not in zh_cn_dict[k]:
-                sql = '''INSERT INTO LANG (modid, en_us, zh_cn)
-                         VALUES ("{}", "{}", "{}")'''.format(modid, en_us_dict[k], zh_cn_dict[k])
-                cursor.execute(sql)
+        if k in en_us_dict and 0 < len(en_us_dict[k]) <= 256 and 0 < len(zh_cn_dict[k]) <= 16 \
+                and '"' not in en_us_dict[k] and '"' not in zh_cn_dict[k] \
+                and '\'' not in en_us_dict[k] and '\'' not in zh_cn_dict[k]:
+            data_list.append('("{}", "{}", "{}")'.format(modid, en_us_dict[k], zh_cn_dict[k]))
 
+# 生成 SQL 语句
+sql = 'INSERT INTO LANG (modid, en_us, zh_cn) VALUES {}'.format(','.join(data_list))
+# 执行 SQL 语句
+cursor.execute(sql)
+# commit 数据
 db.commit()
 # 关闭数据库连接
 db.close()
