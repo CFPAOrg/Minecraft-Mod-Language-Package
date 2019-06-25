8bc42536815cd4d06c6dbc5ed82b9c2a7154b271
Xinyuan Liu <lxy_pku@qq.com>
2019-06-26 06:23:40 +0800

    在解压过程中维护ASSET_MAP

--- src/lang_handle/lang_handle_unzip.py
+++ src/lang_handle/lang_handle_unzip.py
@@ -1,6 +1,8 @@
 import re
 import zipfile
+import json
 
+from src.bytecode_parse.parser import JavaClass
 from src.baka_init import *
 
 
@@ -8,6 +10,9 @@ from src.baka_init import *
 def unzip(unzip_file_name, unzip_path_mods, unzip_path_assets):
     with zipfile.ZipFile(unzip_path_mods + "/" + unzip_file_name, "r") as z:
         logging.info("开始解压 " + z.filename + " 文件")
+        # asset domain和mod id集合，都可能有多个
+        asset_domain=set()
+        mod_id=set()
         # 遍历文件列表
         for i in z.filelist:
             # 找出所有的中英文语言文件
@@ -19,6 +24,10 @@ def unzip(unzip_file_name, unzip_path_mods, unzip_path_assets):
                 zip_tmp_file_path = unzip_path_assets + "/" + i.filename.lower().replace("zh_cn.lang", "zh_cn_old.lang")
                 # 剔除最后 11 字符，这样正好获取得到了文件夹路径
                 zip_tmp_dir_path = unzip_path_assets + "/" + i.filename[:-11]
+
+                # 类似的方法拿到asset domain文件夹名
+                asset_name = i.filename[7:-16]
+                asset_domain.add(asset_name)
 
                 # 先判定文件夹在不在，不在的话创建文件夹
                 # exist_ok 用来跳过文件夹存在错误
@@ -32,14 +41,79 @@ def unzip(unzip_file_name, unzip_path_mods, unzip_path_assets):
                         # 强制重写编码，防止某些作者另辟蹊径
                         tmp.write(f.read())
 
+        # 接下来解析modid
+        # 先尝试从Forge缓存的注释中解析modid
+        fml_cache_annotation_file='META-INF/fml_cache_annotation.json'
+        if fml_cache_annotation_file in z.namelist():
+            with z.open(fml_cache_annotation_file) as f:
+                annotations = json.load(f)
+                for class_ann in annotations.values():
+                    if not 'annotations' in class_ann:
+                        continue
+                    for ann in class_ann['annotations']:
+                        if ann['type'] == 'CLASS' and ann['name'] == 'Lnet/minecraftforge/fml/common/Mod;':
+                            mod_id.add(ann['values']['modid']['value'])
+        
+        # 如果没有缓存的注释文件
+        else: 
+            # 没有asset，也不用再找modid了
+            if len(asset_domain)==0:
+                return (mod_id,asset_domain)
+
+            # 解析所有class文件的常量池，找modid
+            success=True
+            for i in z.filelist:
+                if i.filename.endswith('.class'):
+                    with z.open(i.filename,'r') as f:
+                        # 这个disassembler功能不是很完善，可能会报错
+                        try:
+                            jc = JavaClass(f.read())
+                            strings = jc.get_constant_string()
+                            for i in range(len(strings) - 2):
+                                if strings[i] == 'Lnet/minecraftforge/fml/common/Mod;' and strings[i+1] == 'modid':
+                                    mod_id.add(strings[i+2])    
+                        except:
+                            success = False
+            
+            # 没有报错，返回
+            if success:
+                return (mod_id,asset_domain)
+
+            # 有报错，但至少找到了一个modid
+            elif len(mod_id)!=0:
+                # TODO: log
+                # 在绝大多数使用场景下，我们并不需要找到一个jar包里所有注册的mod的id，所以这里可以直接返回
+                # TODO: 用config控制这里的行为
+                return (mod_id,asset_domain)
+
+            # 虽然mcmod.info往往不能信...但现在我们走投无路了
+            if 'mcmod.info' in z.namelist():
+                with z.open('mcmod.info') as f:
+                    try:
+                        modinfo=json.load(f)
+                        for info in modinfo:
+                            mod_id.add(info['modid'])
+                    except:
+                        # TODO: log
+                        pass
+            
+    return (mod_id,asset_domain)
+
 
 def main(path_mods, path_assets):
     logging.info("==================  解压部分开始  ==================")
-    # 开始遍历模组临时文件夹，解压得到内部的语言文件
+    # 开始遍历模组临时文件夹，解压得到内部的语言文件，并更新ASSET_MAP
     for zf in os.listdir(path_mods):
         # 如果是 zip 文件，进行解压
         if zipfile.is_zipfile(path_mods + "/" + zf):
-            unzip(zf, path_mods, path_assets)
+            mod_id,asset_domain = unzip(zf, path_mods, path_assets)
+            if len(asset_domain) != 0:
+                for modid in mod_id:
+                    if not modid in ASSET_MAP:
+                        ASSET_MAP[modid]=[]
+                    for domain in asset_domain:
+                        if not domain in ASSET_MAP[modid]:
+                            ASSET_MAP[modid].append(domain)
     logging.info("==================  解压部分结束  ==================")
 
 
