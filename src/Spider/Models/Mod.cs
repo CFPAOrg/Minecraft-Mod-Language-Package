59cb86ba496f1bf338132b7b7716b503de6f50cb
Summpot <summpot@outlook.com>
2020-01-30 22:16:32 +0800

    添加爬虫，清理目录  (#637)
    * cleanup
    
    * Create packer.yml
    
    * Create spider.yml
    
    * Delete pack.yml
    
    * update
    
    * Delete mod_list.json
    
    * Delete curse_project.json

---
+++ src/Spider/Models/Mod.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Spider.Models
+{
+    internal class Mod
+    {
+        private bool _isInBlackList;
+
+        public Mod(long id)
+        {
+            Id = id;
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string ShortUrl => Url[(Url.LastIndexOf('/') + 1)..];
+        public string DownloadUrl { get; set; }
+        public List<Language> Languages { get; set; } = new List<Language>();
+
+        public bool IsInBlackList
+        {
+            get
+            {
+                if (Languages.Count == 0) return _isInBlackList;
+                return Languages.All(_ => _.IsInBlackList) || _isInBlackList;
+            }
+            set => _isInBlackList = value;
+        }
+
+        public Stream Stream { get; set; }
+    }
+}
