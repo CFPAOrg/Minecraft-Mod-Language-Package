db6b5bee15fefbc1792f9bae8bec1802e94f5ad5
Summpot <summpot@outlook.com>
2020-08-10 21:07:11 +0800

    添加信号量

--- src/Spider/ModManager.cs
+++ src/Spider/ModManager.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Spider.Properties;
@@ -54,12 +55,14 @@ namespace Spider
 
         public async Task<List<Mod>> GetModIdAsync(IEnumerable<Mod> mods)
         {
+            SemaphoreSlim semaphore = new SemaphoreSlim(0,20);
             File.WriteAllBytes("./cfr.jar", Resources.cfr_0_150);
             var regex = new Regex("(?<=modid=\").*?(?=\")");
             var result = mods.Select(_ =>
             {
-                return Task.Run(() =>
+                return Task.Run(async () =>
                 {
+                    await semaphore.WaitAsync();
                     _logger.LogInformation($"开始对 {_.Path} 进行反编译以获得modid.");
                     var process = new Process
                     {
@@ -88,6 +91,7 @@ namespace Spider
 
                     _logger.LogCritical($"找到了modid: {modid}");
                     _.ModId = modid;
+                    semaphore.Release(1);
                     return _;
                 });
             });
