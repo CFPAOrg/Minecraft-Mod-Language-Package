81893f2ba466ed7f66672964a0efc33ba443808e
dovisutu <40313014+dovisutu@users.noreply.github.com>
2020-08-12 21:46:53 +0800

    Add packer
    说实话，这个逻辑简单到我怀疑自己哪里写错了（

--- src/Packer/Program.cs
+++ src/Packer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using Serilog;
 
 namespace Packer
 {
@@ -6,7 +10,21 @@ namespace Packer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("114514");
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            var config = RetrieveConfig();
+            Log.Information("Starting packaging......");
+            // 可以改这个zip的名字吗？有可能是breaking
+            ZipFile.CreateFromDirectory($"./projects/langresource/{config.Version}", "./Minecraft-Mod-Language-Package.zip");
+            Log.Information("Successfully wrote the pack to ./Minecraft-Mod-Language-Package.zip .")
+        }
+
+        static Config RetrieveConfig()
+        {
+            var reader = File.ReadAllBytes("./config/packer.json");
+            return JsonSerializer.Deserialize<Config>(reader);
         }
     }
 }
