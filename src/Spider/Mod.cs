b66b1e273bf5c85c6f7c522c0267b071658c64ab
Summpot <summpot@outlook.com>
2020-08-13 12:18:52 +0800

    FIx

--- src/Spider/Mod.cs
+++ src/Spider/Mod.cs
@@ -5,7 +5,7 @@ using System.Text.Json.Serialization;
 
 namespace Spider
 {
-    public class Mod
+    public class Mod:IEquatable<Mod>
     {
         [JsonPropertyName("name")]
         public string Name { get; set; }
@@ -27,5 +27,42 @@ namespace Spider
         public List<string> LanguageFilePaths { get; set; }
         [JsonIgnore]
         public string Path { get; set; }
+
+        public bool Equals(Mod other)
+        {
+            return ProjectId == other.ProjectId;
+        }
+
+        private sealed class ProjectIdEqualityComparer : IEqualityComparer<Mod>
+        {
+            public bool Equals(Mod x, Mod y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.ProjectId == y.ProjectId;
+            }
+
+            public int GetHashCode(Mod obj)
+            {
+                return obj.ProjectId.GetHashCode();
+            }
+        }
+
+        public static IEqualityComparer<Mod> ProjectIdComparer { get; } = new ProjectIdEqualityComparer();
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Mod) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ProjectId.GetHashCode();
+        }
     }
 }
