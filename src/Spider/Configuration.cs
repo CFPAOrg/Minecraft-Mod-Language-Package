e0d88965a51b69a4fb73c3e42428704af4ce120f
Summpot <summpot@outlook.com>
2020-08-14 17:49:36 +0800

    2400

--- src/Spider/Configuration.cs
+++ src/Spider/Configuration.cs
@@ -13,7 +13,7 @@ namespace Spider
         public static Configuration Default { get; }=new Configuration()
         {
             EnabledGameVersions = new List<string>{"1.12.2"},
-            ModCount = 2000,
+            ModCount = 2400,
             ModInfoPath = "./mod_info.json"
         };
 
