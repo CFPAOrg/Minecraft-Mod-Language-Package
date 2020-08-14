b370582d780d764563bd79dd023a1848de0cb9da
dovisutu <40313014+dovisutu@users.noreply.github.com>
2020-08-14 19:27:58 +0800

    Fix Packer
    只不过 1old 里的东西不知道怎么处理...

--- src/Packer/Config.cs
+++ src/Packer/Config.cs
@@ -9,5 +9,8 @@ namespace Packer
     {
         [JsonPropertyName("targetVersion")]
         public string Version { get; set; }
+
+        [JsonPropertyName("blockList")]
+        public List<string> BlockList { get; set; }
     }
 }
--- src/Packer/Program.cs
+++ src/Packer/Program.cs
@@ -18,14 +18,19 @@ namespace Packer
                 .CreateLogger();
             var config = RetrieveConfig();
             Log.Information("开始打包。版本：{0}", config.Version);
-            using var stream = File.Create("./Minecraft-Mod-Language-Package"); // 生成空 zip 文档
-            using var archive = new ZipArchive(stream);
+            using var stream = File.Create(".\\Minecraft-Mod-Language-Package.zip"); // 生成空 zip 文档
+            using var archive = new ZipArchive(stream, ZipArchiveMode.Update);
             InitializeArchive(archive, config);
             var existingModids = new HashSet<string>();
-            var assetsToBePacked = new DirectoryInfo($"./projects/{config.Version}")
+            var assetsToBePacked = new DirectoryInfo($".\\projects\\{config.Version}\\assets")
                 .EnumerateDirectories()
                 .Select(_ =>
                 {
+                    if (config.BlockList.Contains(_.Name))
+                    {
+                        Log.Information("跳过一个黑名单中的目录：{0}", _.Name);
+                        return null;
+                    }
                     var descend = _.GetDirectories()[0]; // 在目前的格式下 modid-projectid 下只有一个文件夹，表示 asset-domain
                     Log.Information("将 {0}（asset-domain：{1}）加入待打包的清单", _.Name, descend.Name);
                     return new { modid = descend.Name, 
@@ -34,14 +39,18 @@ namespace Packer
                 });
             foreach (var asset in assetsToBePacked)
             {
+                if(asset is null)
+                {
+                    continue;
+                }
                 var id = asset.modid;
                 Log.Information("正在打包 asset-domain：{0}", id);
                 if (existingModids.Contains(id))
                 {
                     Log.Warning("检测到此 asset-domain 与其他 mod 重合");
-                    foreach(var file in asset.assetPath.EnumerateFiles())
+                    foreach(var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
                     {
-                        var destinationPath = $"assets/{id}/{file.FullName[asset.prefixLength..]}";
+                        var destinationPath = $"assets\\{file.FullName[(asset.prefixLength + 1)..]}";
                         var existingFile = archive.GetEntry(destinationPath);
                         if(existingFile is null) // null 代表没有找到文件，也就是该文件没有重合
                         {
@@ -61,9 +70,9 @@ namespace Packer
                 }
                 else
                 {
-                    foreach (var file in asset.assetPath.EnumerateFiles())
+                    foreach (var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
                     {
-                        var destinationPath = $"assets/{id}/{file.FullName[asset.prefixLength..]}";
+                        var destinationPath = $"assets\\{file.FullName[(asset.prefixLength + 1)..]}";
                         archive.CreateEntryFromFile(file.FullName, destinationPath);
                         Log.Information("添加了 {0}", destinationPath);
                     }
@@ -74,17 +83,17 @@ namespace Packer
 
         static void InitializeArchive(ZipArchive archive, Config config)
         {
-            string common = $"./projects/{config.Version}";
+            string common = $".\\projects\\{config.Version}";
             new List<string>() { "LICENSE", "pack.mcmeta", "pack.png", "README.md" }.ForEach(_ =>
               {
                   Log.Information("初始化压缩包：添加 {0}", _);
-                  archive.CreateEntryFromFile($"{common}/{_}", _);
+                  archive.CreateEntryFromFile($"{common}\\{_}", _);
               });
         }
 
         static Config RetrieveConfig()
         {
-            var reader = File.ReadAllBytes("./config/packer.json");
+            var reader = File.ReadAllBytes(".\\config\\packer.json");
             return JsonSerializer.Deserialize<Config>(reader);
         }
     }
