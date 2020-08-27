821a8cd222ff556c3d44d6e22ae68a242ca50ed2
Nullpinter <fanng233@gmail.com>
2020-08-27 15:22:36 +0800

    添加uploader

---
+++ src/Uploader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Renci.SshNet;
+using Serilog;
+
+namespace Uploader
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            var host = args[0];
+            var name = args[1];
+            var pwd = args[2];
+            using var client = new ScpClient(host, 22, name, pwd);
+            client.Connect();
+            if (client.IsConnected)
+            {
+                Log.Logger.Information("服务器连接成功！");
+            }
+            else
+            {
+                Log.Logger.Error("服务器连接失败！");
+            }
+            var fs = File.OpenRead("./Minecraft-Mod-Language-Package.zip");
+            client.Upload(fs, "/var/www/html/files/Minecraft-Mod-Language-Modpack.zip");
+            client.Disconnect();
+        }
+    }
+}
