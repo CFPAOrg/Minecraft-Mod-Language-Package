bd5bfa209fa3cd8f790310ec7cee815d18b6c8cd
Nullpinter <fanng233@gmail.com>
2020-08-29 15:02:26 +0800

    更改服务器上传ip

--- src/Uploader/Program.cs
+++ src/Uploader/Program.cs
@@ -11,7 +11,7 @@ namespace Uploader
             var host = args[0];
             var name = args[1];
             var pwd = args[2];
-            using var client = new ScpClient(host, 22, name, pwd);
+            using var client = new ScpClient(host, 12356, name, pwd);
             client.Connect();
             if (client.IsConnected)
             {
