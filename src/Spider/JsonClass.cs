3cc90be53165b86cb5e366cd234170c7402bd560
Nullpinter <fanng233@gmail.com>
2020-09-07 18:18:18 +0800

    reborn

---
+++ src/Spider/JsonClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Spider {
+    public class Conf {
+        [JsonPropertyName("version")] public string Version { get; set; }
+        [JsonPropertyName("base_mod_count")] public long ModCount { get; set; }
+        [JsonPropertyName("black_list")] public List<string> BlackList { get; set; }
+        [JsonPropertyName("white_list")] public List<string> WhiteList { get; set; }
+    }
+
+    public class ScanConf {
+        [JsonPropertyName("black_list")] public List<string> BlackList { get; set; }
+        [JsonPropertyName("white_list")] public List<string> WhiteList { get; set; }
+    }
+
+    public class Info {
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("project_name")] public string ProjectName { get; set; }
+        [JsonPropertyName("project_id")] public long ProjectId { get; set; }
+        [JsonPropertyName("mod_id")] public string ModId { get; set; }
+        [JsonPropertyName("mod_domain")] public List<string> Domain { get; set; }
+        [JsonPropertyName("has_language_folder")]
+        public bool HasLang { get; set; }
+    }
+}
---
+++ src/Spider/Urllib.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Spider {
+    public class Urllib {
+        public static async Task<List<Addon>> GetModInfoAsync(int modCount, string gameVersion)
+        {
+            using var httpClient = new HttpClient();
+            var uriBuilder = new UriBuilder("https://addons-ecs.forgesvc.net/api/v2/addon/search")
+            {
+                Query =
+                    $"categoryId=0&gameId=432&index=0&pageSize={modCount}&gameVersion={gameVersion}&sectionId=6&sort=1"
+            };
+            return await httpClient.GetFromJsonAsync<List<Addon>>(uriBuilder.Uri);
+        }
+    }
+}
