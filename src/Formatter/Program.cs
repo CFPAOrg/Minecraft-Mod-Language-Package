2cc4324550577e503daa92ad80331fb307e73c79
Nullpinter <fanng233@gmail.com>
2020-10-17 11:14:40 +0800

    挂上格式化-1

---
+++ src/Formatter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Formatter
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = await JsonSerializer.DeserializeAsync<Configuration[]>(File.OpenRead("./config/spider/config.json"));
+            foreach (var configuration in config) {
+                var file = Util.SearchLangFiles(configuration.Version);
+                await Util.FormatLangFile(file);
+            }
+        }
+    }
+}
