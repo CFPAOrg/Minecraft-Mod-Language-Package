8806f56274ec8a3d6fa9a31f27b121292a5d0262
dovisutu <40313014+dovisutu@users.noreply.github.com>
2020-10-17 23:04:04 +0800

    Tweak Packer
    写作 Tweak，读作 中改
    进行了以下更改：
     - 向配置添加了更多选项，方便跨版本的协调
     - 把一些方法提到了另一个类，以免 Program.cs 过于臃肿
     - 对文件合并写了一个真的逻辑，以免合并的 json 直接炸掉

--- src/Packer/Program.cs
+++ src/Packer/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Linq;
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Text.Json;
-using Serilog;
-using System.Collections.Generic;
 
+using static Packer.Utils;
 
 namespace Packer
 {
@@ -22,13 +23,13 @@ namespace Packer
             using var stream = File.Create(".\\Minecraft-Mod-Language-Package.zip"); // 生成空 zip 文档
             using var archive = new ZipArchive(stream, ZipArchiveMode.Update);
             InitializeArchive(archive, config);
-            var existingModids = new Dictionary<string, string>();
+            var existingDomains = new Dictionary<string, string>();
             var assetsToBePacked = new DirectoryInfo($".\\projects\\{config.Version}\\assets")
                 .EnumerateDirectories()
                 .SelectMany(_ => _.GetDirectories()
                     .Select(descend => new
                     {
-                        modid = descend.Name,
+                        domainName = descend.Name,
                         name = _.Name,
                         assetPath = descend,
                         prefixLength = _.FullName.Length
@@ -36,17 +37,27 @@ namespace Packer
                 );
             foreach (var asset in assetsToBePacked)
             {
-                if(asset is null)
+                if (asset is null)
                 {
                     continue;
                 }
-                var id = asset.modid;
+                var domain = asset.domainName;
                 var name = asset.name;
-                Log.Information("正在打包 {0}（asset-domain：{1}）", name, id);
-                if (existingModids.ContainsKey(id))
+                if (config.ModBlackList.Contains(name))
                 {
-                    Log.Warning("检测到 asset-domain 与 {0} 重合", existingModids[id]);
-                    foreach(var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
+                    Log.Information("跳过了黑名单中的 mod：{0}（asset-domain：{1}", name, domain);
+                    continue;
+                }
+                if (config.DomainBlackList.Contains(domain))
+                {
+                    Log.Information("跳过了黑名单中的 asset-domain：{0}（对应 mod：{1}）", domain, name);
+                    continue;
+                }
+                Log.Information("正在打包 {0}（asset-domain：{1}）", name, domain);
+                if (existingDomains.ContainsKey(domain))
+                {
+                    Log.Warning("检测到 asset-domain 与 {0} 重合", existingDomains[domain]);
+                    foreach (var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
                     {
                         if (file.FullName.Contains("en_us", StringComparison.OrdinalIgnoreCase))
                         {
@@ -57,7 +68,7 @@ namespace Packer
                             .Replace("zh_CN", "zh_cn") // 修复大小写
                             .Replace('\\', '/'); // 修复 Java 平台读取 CentralDirectory 部分时正反斜杠的问题
                         var existingFile = archive.GetEntry(destinationPath);
-                        if(existingFile is null) // null 代表没有找到文件，也就是该文件没有重合
+                        if (existingFile is null) // null 代表没有找到文件，也就是该文件没有重合
                         {
                             archive.CreateEntryFromFile(file.FullName, destinationPath);
                             Log.Information("添加了暂未重合的 {0}", destinationPath);
@@ -67,8 +78,7 @@ namespace Packer
                             Log.Warning("检测到重合文件：{0}", destinationPath);
                             using var reader = File.OpenRead(file.FullName);
                             using var writer = existingFile.Open();
-                            writer.Seek(0, SeekOrigin.End); // 置于末尾
-                            reader.CopyTo(writer); // 默认是有缓冲区的，不用担心爆内存
+                            CombineLangFiles(writer, reader, file.Extension);
                             Log.Information("完成合并");
                         }
                     }
@@ -88,23 +98,11 @@ namespace Packer
                         archive.CreateEntryFromFile(file.FullName, destinationPath);
                         Log.Information("添加了 {0}", destinationPath);
                     }
-                    Log.Information("向 asset-domain 映射表中加入：{0} -> {1}", id, name);
-                    existingModids.Add(id, name);
+                    Log.Information("向 asset-domain 映射表中加入：{0} -> {1}", domain, name);
+                    existingDomains.Add(domain, name);
                 }
             }
             Log.Information("打包结束");
-        }
-
-        static void InitializeArchive(ZipArchive archive, Config config)
-        {
-            Log.Information("开始初始化压缩包");
-            string common = $".\\projects\\{config.Version}";
-            new List<string>() { "LICENSE", "pack.mcmeta", "pack.png", "README.md" }.ForEach(_ =>
-              {
-                  Log.Information("初始化压缩包：添加 {0}", _);
-                  archive.CreateEntryFromFile($"{common}\\{_}", _, CompressionLevel.Fastest);
-              });
-            Log.Information("初始化完成");
         }
 
         static Config RetrieveConfig()
