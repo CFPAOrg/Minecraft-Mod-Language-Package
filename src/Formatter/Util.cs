05329a0b40a66a535f94db19aa8968f2147b22f2
Nullpinter <fanng233@gmail.com>
2020-11-13 18:21:58 +0800

    remove add

--- src/Formatter/Util.cs
+++ src/Formatter/Util.cs
@@ -37,6 +37,7 @@ namespace Formatter {
                 var value = kv[1];
                 ht.Add(key, value);
             }
+
             return ht;
         }
 
@@ -52,10 +53,12 @@ namespace Formatter {
                         ls.Add(str);
                         Log.Debug("添加空行");
                     }
+
                     if (commentReg.IsMatch(str)) {
                         ls.Add(str);
                         Log.Debug("添加规范注释");
                     }
+
                     if (findEqual.IsMatch(str)) {
                         if (keyReg.IsMatch(str)) {
                             if (bl.Contains(keyReg.Match(str).ToString())) continue;
@@ -68,6 +71,7 @@ namespace Formatter {
                         }
                     }
                 }
+
                 await File.WriteAllLinesAsync(l, ls);
                 Log.Information("{0}检查完成", l);
             }
@@ -75,14 +79,16 @@ namespace Formatter {
 
         public static async Task CrateEmptyLangFile(string version) {
             var root = new DirectoryInfo($"./projects/{version}/assets");
-            var dt1 = root.GetDirectories();//顶层project name
+            var dt1 = root.GetDirectories(); //顶层project name
             foreach (var info in dt1) {
-                var dt2 = info.GetDirectories();//domain
+                var dt2 = info.GetDirectories(); //domain
                 foreach (var directoryInfo in dt2) {
                     try {
                         var lf = Directory.EnumerateFiles(Path.Combine(directoryInfo.FullName, "lang"));
-                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_us.lang")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_US.lang"))) {
-                            if (!(lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.lang")))) {
+                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_us.lang")) ||
+                            lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_US.lang"))) {
+                            if (!(lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang")) ||
+                                  lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.lang")))) {
                                 var pa = lf.FirstOrDefault(_ => _.ToLower().Contains("en_us.lang"));
                                 var isParse = false;
                                 foreach (string str in await File.ReadAllLinesAsync(pa, Encoding.UTF8)) {
@@ -93,15 +99,20 @@ namespace Formatter {
                                 }
 
                                 if (isParse) {
-                                    await File.WriteAllTextAsync(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang"), "#PARSE_ESCAPES\n\n");
+                                    await File.WriteAllTextAsync(
+                                        Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang"),
+                                        "#PARSE_ESCAPES\n\n");
                                     Log.Logger.Information($"为{directoryInfo.Name}新建带#PARSE_ESCAPES的文件");
-                                } else {
-                                    await File.WriteAllTextAsync(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang"), "\n");
+                                }
+                                else {
+                                    await File.WriteAllTextAsync(
+                                        Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang"), "\n");
                                     Log.Logger.Information($"为{directoryInfo.Name}新建空文件");
                                 }
                             }
                         }
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         Log.Logger.Error(e.Message);
                     }
                 }
@@ -110,18 +121,22 @@ namespace Formatter {
 
         public static async Task CrateEmptyJsonFile(string version) {
             var root = new DirectoryInfo($"./projects/{version}/assets");
-            var dt1 = root.GetDirectories();//顶层project name
+            var dt1 = root.GetDirectories(); //顶层project name
             foreach (var info in dt1) {
-                var dt2 = info.GetDirectories();//domain
+                var dt2 = info.GetDirectories(); //domain
                 foreach (var directoryInfo in dt2) {
                     try {
                         var lf = Directory.EnumerateFiles(Path.Combine(directoryInfo.FullName, "lang"));
-                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_us.json")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_US.json"))) {
-                            if (!(lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.json")))) {
-                                await File.WriteAllTextAsync(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json"), "{}");
+                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_us.json")) ||
+                            lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "en_US.json"))) {
+                            if (!(lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json")) ||
+                                  lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.json")))) {
+                                await File.WriteAllTextAsync(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json"),
+                                    "{}");
                             }
                         }
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         Log.Logger.Error(e.Message);
                     }
                 }
@@ -130,13 +145,14 @@ namespace Formatter {
 
         public static async Task ReplaceFontInLang(string version, Hashtable map) {
             var root = new DirectoryInfo($"./projects/{version}/assets");
-            var dt1 = root.GetDirectories();//顶层project name
+            var dt1 = root.GetDirectories(); //顶层project name
             foreach (var info in dt1) {
-                var dt2 = info.GetDirectories();//domain
+                var dt2 = info.GetDirectories(); //domain
                 foreach (var directoryInfo in dt2) {
                     try {
                         var lf = Directory.EnumerateFiles(Path.Combine(directoryInfo.FullName, "lang"));
-                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.lang"))) {
+                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.lang")) ||
+                            lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.lang"))) {
                             var pa = lf.FirstOrDefault(_ => _.ToLower().Contains("zh_cn.lang"));
                             var isParse = false;
                             foreach (string str in await File.ReadAllLinesAsync(pa, Encoding.UTF8)) {
@@ -164,8 +180,10 @@ namespace Formatter {
                                             var skey = key as string;
                                             line = line.Replace(skey, map[skey] as string);
                                         }
+
                                         replaced.Add(line);
-                                    } else {
+                                    }
+                                    else {
                                         replaced.Add(str);
                                     }
                                 }
@@ -173,7 +191,8 @@ namespace Formatter {
                                 await File.WriteAllLinesAsync(pa, replaced);
                             }
                         }
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         Log.Logger.Error(e.Message);
                     }
                 }
@@ -182,13 +201,14 @@ namespace Formatter {
 
         public static async Task ReplaceFontInJson(string version, Hashtable map) {
             var root = new DirectoryInfo($"./projects/{version}/assets");
-            var dt1 = root.GetDirectories();//顶层project name
+            var dt1 = root.GetDirectories(); //顶层project name
             foreach (var info in dt1) {
-                var dt2 = info.GetDirectories();//domain
+                var dt2 = info.GetDirectories(); //domain
                 foreach (var directoryInfo in dt2) {
                     try {
-                        var lf = Directory.EnumerateFiles(Path.Combine(directoryInfo.FullName, "json"));
-                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json")) || lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.json"))) {
+                        var lf = Directory.EnumerateFiles(Path.Combine(directoryInfo.FullName, "lang"));
+                        if (lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_cn.json")) ||
+                            lf.Contains(Path.Combine(directoryInfo.FullName, "lang", "zh_CN.json"))) {
                             var pa = lf.FirstOrDefault(_ => _.ToLower().Contains("zh_cn.json"));
 
                             var replaced = new List<string>();
@@ -208,8 +228,10 @@ namespace Formatter {
                                         var skey = key as string;
                                         line = line.Replace(skey, map[skey] as string);
                                     }
+
                                     replaced.Add(line);
-                                } else {
+                                }
+                                else {
                                     replaced.Add(str);
                                 }
 
@@ -217,9 +239,13 @@ namespace Formatter {
                                 await File.WriteAllLinesAsync(pa, replaced);
                             }
                         }
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         Log.Logger.Error(e.Message);
                     }
                 }
             }
         }
+    }
+}
+        
