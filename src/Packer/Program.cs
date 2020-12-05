5fd546f2c6cb71a3393e1385e98e12a33d407659
dovisutu <40313014+dovisutu@users.noreply.github.com>
2020-12-05 23:17:26 +0800

    Update Packer
    进行了一次重构
    主要改动：
     - 把一些方法提取出来了，如将一个语言文件加入压缩包
     - 把元素名称的预处理从 Formatter 里提出来了（虽然原来那份还在）
     - 添加了一点点 Log

--- src/Packer/Program.cs
+++ src/Packer/Program.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Formatter;
 using static Packer.Utils;
 
 namespace Packer
@@ -19,16 +19,8 @@ namespace Packer
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
                 .CreateLogger();
-            var hashmap = await Util.ReadReplaceFontMap();
+            var mapping = await ReadReplaceFontMap();
             var config = RetrieveConfig();
-            if (config.Version == "1.12.2")
-            {
-                await Util.ReplaceFontInLang(config.Version, hashmap);
-            }
-            else
-            {
-                await Util.ReplaceFontInJson(config.Version, hashmap);
-            }
             Log.Information("开始打包。版本：{0}", config.Version);
             using var stream = File.Create(".\\Minecraft-Mod-Language-Package.zip"); // 生成空 zip 文档
             using var archive = new ZipArchive(stream, ZipArchiveMode.Update);
@@ -64,50 +56,51 @@ namespace Packer
                     continue;
                 }
                 Log.Information("正在打包 {0}（asset-domain：{1}）", name, domain);
-                if (existingDomains.ContainsKey(domain))
+                bool conflict = existingDomains.ContainsKey(domain);
+                if (conflict)
                 {
                     Log.Warning("检测到 asset-domain 与 {0} 重合", existingDomains[domain]);
-                    foreach (var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
+                }
+                foreach (var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
+                {
+                    if (file.FullName.Contains("en_us", StringComparison.OrdinalIgnoreCase))
                     {
-                        if (file.FullName.Contains("en_us", StringComparison.OrdinalIgnoreCase))
-                        {
-                            Log.Information("跳过了英文原文：{0}", file.FullName[(asset.prefixLength + 1)..]);
-                            continue;
-                        }
-                        var destinationPath = $"assets\\{file.FullName[(asset.prefixLength + 1)..]}"
-                            .Replace("zh_CN", "zh_cn") // 修复大小写
-                            .Replace('\\', '/'); // 修复 Java 平台读取 CentralDirectory 部分时正反斜杠的问题
+                        Log.Information("跳过了英文原文：{0}", file.FullName[(asset.prefixLength + 1)..]);
+                        continue;
+                    }
+                    var destinationPath = $"assets\\{file.FullName[(asset.prefixLength + 1)..]}"
+                        .Replace("zh_CN", "zh_cn") // 修复大小写
+                        .Replace('\\', '/'); // 修复 Java 平台读取 CentralDirectory 部分时正反斜杠的问题
+                    var fileContent = File.ReadAllText(file.FullName, Encoding.UTF8)
+                        .Preprocess(file.Extension, mapping);
+
+                    if (conflict)
+                    {
                         var existingFile = archive.GetEntry(destinationPath);
                         if (existingFile is null) // null 代表没有找到文件，也就是该文件没有重合
                         {
-                            archive.CreateEntryFromFile(file.FullName, destinationPath);
+                            archive.CreateLangFile(destinationPath, fileContent);
                             Log.Information("添加了暂未重合的 {0}", destinationPath);
                         }
                         else
                         {
                             Log.Warning("检测到重合文件：{0}", destinationPath);
-                            using var reader = File.OpenRead(file.FullName);
+                            // 如果都传 string 进去，不如顺便把合并系统做成基于 Parse 的，但是有需求了再弄吧
+                            using var reader = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
                             using var writer = existingFile.Open();
                             CombineLangFiles(writer, reader, file.Extension);
                             Log.Information("完成合并");
+                            
                         }
                     }
-                }
-                else
-                {
-                    foreach (var file in asset.assetPath.EnumerateFiles("*", SearchOption.AllDirectories))
+                    else
                     {
-                        if (file.FullName.Contains("en_us", StringComparison.OrdinalIgnoreCase))
-                        {
-                            Log.Information("跳过了英文原文：{0}", file.FullName[(asset.prefixLength + 1)..]);
-                            continue;
-                        }
-                        var destinationPath = $"assets\\{file.FullName[(asset.prefixLength + 1)..]}"
-                            .Replace("zh_CN", "zh_cn") // 修复大小写
-                            .Replace('\\', '/'); // 修复 Java 平台读取 CentralDirectory 部分时正反斜杠的问题
-                        archive.CreateEntryFromFile(file.FullName, destinationPath);
+                        archive.CreateLangFile(destinationPath, fileContent);
                         Log.Information("添加了 {0}", destinationPath);
                     }
+                }
+                if (!conflict)
+                {
                     Log.Information("向 asset-domain 映射表中加入：{0} -> {1}", domain, name);
                     existingDomains.Add(domain, name);
                 }
--- src/Packer/Utils.cs
+++ src/Packer/Utils.cs
@@ -1,11 +1,53 @@
 ﻿using Serilog;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Packer
 {
     static class Utils
     {
+        public static string Preprocess(this string file, string extension, Dictionary<string, string> mappings)
+        { // 不知道需不需要 PostProcess...反正现在即使要写也是一个空方法
+            if (!new List<string> { ".lang", ".json", ".txt" }.Contains(extension))
+            {
+                Log.Warning("检测到未预料到的拓展名：{0}", extension);
+            }
+            foreach (var mapping in mappings)
+            {
+                if (file.Contains(mapping.Key))
+                {
+                    Log.Information("正在进行特殊符号替换：{0} -> {1}", mapping.Key, mapping.Value);
+                }
+                switch (extension)
+                {
+                    case ".lang":
+                    case ".txt":
+                        file.Replace(mapping.Key, Regex.Unescape(mapping.Value));
+                        break;
+                    case ".json":
+                        file.Replace(mapping.Key, mapping.Value);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return file;
+        }
+
+        public static void CreateLangFile(this ZipArchive archive, string destination, string content)
+        {
+            using var writer = new StreamWriter(
+                archive.CreateEntry(destination)
+                       .Open(),
+                Encoding.UTF8);
+            writer.Write(content);
+            writer.Flush(); // 确保一下
+        }
+
         public static void CombineLangFiles(Stream destination, Stream added, string extension)
         {
             // 需要注意的是，不同格式的文件需要不同的处理，因此需要传入拓展名
@@ -45,14 +87,28 @@ namespace Packer
         public static void InitializeArchive(ZipArchive archive, Config config)
         {
             Log.Information("开始初始化压缩包");
-            string commonPrefix = $".\\projects\\{config.Version}";
+            string commonPrefix = $"./projects/{config.Version}";
             config.FilesToInitialize.ForEach(_ =>
             {
-                Log.Information("初始化压缩包：添加 {0}", _);
-                archive.CreateEntryFromFile($"{commonPrefix}\\{_}", _, CompressionLevel.Fastest);
+                var destination = _.Replace("/1UNKNOWN", ""); // 除掉一层文件夹（在 assets/ 里的各种 fix）
+                Log.Information("初始化压缩包：添加 {0}", _.Replace("/1UNKNOWN",""));
+                archive.CreateEntryFromFile($"{commonPrefix}/{_}", _.Replace("/1UNKNOWN", ""));
             });
 
             Log.Information("初始化完成");
+        }
+
+        public static async Task<Dictionary<string, string>> ReadReplaceFontMap() // 从隔壁弄过来改了一下，就放这里了
+        {
+            var mapping = new Dictionary<string, string>();
+            foreach (string str in await File.ReadAllLinesAsync("./config/fontmap.txt", Encoding.UTF8))
+            {
+                var kv = str.Split('>', 2);
+                var key = kv[0];
+                var value = kv[1];
+                mapping.Add(key, value);
+            }
+            return mapping;
         }
     }
 }
