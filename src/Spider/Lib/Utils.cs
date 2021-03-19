597a882dc93145f8a0f542ab798b471ad8824d3a
Nullpinter <fanng233@gmail.com>
2021-03-19 20:12:55 +0800

    rua

--- src/Spider/Lib/Utils.cs
+++ src/Spider/Lib/Utils.cs
@@ -3,15 +3,22 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Language.Core;
 using Serilog;
+
 using Spider.Lib.JsonLib;
 
-namespace Spider.Lib {
-    public class Utils {
-        public static async Task ParseMods((ModInfo,Configuration) tuple) {
+namespace Spider.Lib
+{
+    public class Utils
+    {
+        public static async Task ParseMods((ModInfo, Configuration) tuple)
+        {
 
-            if (tuple.Item2.NonUpdate) {
+            if (tuple.Item2.NonUpdate)
+            {
                 return;
             }
 
@@ -26,7 +33,8 @@ namespace Spider.Lib {
                 var file = mod.GameVersionLatestFiles.First(_ => _.GameVersion == version);
                 downloadUrl = UrlLib.GetDownloadUrl(file.ProjectFileId.ToString(), file.ProjectFileName);
             }
-            if (downloadUrl is not null) {
+            if (downloadUrl is not null)
+            {
 
                 try
                 {
@@ -51,27 +59,163 @@ namespace Spider.Lib {
             }
         }
 
-        public static void ParseFiles(Mod mod, Configuration cfg,string rootPath) {
+        public static void ParseFiles(Mod mod, Configuration cfg, string rootPath)
+        {
             var zipArchive = new ZipArchive(File.OpenRead(mod.TempPath));
             var tmpDirectories = $"{Path.GetTempPath()}extracted\\{Path.GetFileName(mod.TempPath)}";
             Log.Logger.Debug(tmpDirectories);
-            zipArchive.ExtractToDirectory(tmpDirectories,true);
+            zipArchive.ExtractToDirectory(tmpDirectories, true);
             var directories = cfg.IncludedPath.ToList()
                 .Select(_ => new DirectoryInfo($"{tmpDirectories}\\{_}"));
             var include = cfg.ExtractPath.ToList();
-            foreach (var info in directories) {
-                foreach (var directory in info.GetDirectories()) {
-                    if (directory.Name == "minecraft") {
-                        continue;
+            //遍历所有目录
+            var root = directories.Select(dire =>
+                dire.EnumerateDirectories().Where(domain => domain.Name != "minecraft")
+                    .Select(domain => domain.EnumerateDirectories()
+                        .Where(_ => include.Contains(_.Name))));
+            //遍历遍历所有输出的目录
+            foreach (var a in root)
+            {
+                foreach (var b in a)
+                {
+                    foreach (var c in b)
+                    {
+                        Console.WriteLine(c.FullName);
+                        var dire1 = c.GetDirectories();
+                        if (dire1.Length > 0) {
+                            foreach (var dire2 in dire1)
+                            {
+                                var dire3 = dire2.GetDirectories();
+                                foreach (var info in dire3)
+                                {
+                                    if (info.Name == "en_us")
+                                    {
+                                        Console.WriteLine(info.FullName);
+                                        var p = $"{rootPath}{info.FullName[(info.FullName.LastIndexOf(Path.GetFileName(mod.TempPath)!, StringComparison.Ordinal) + Path.GetFileName(mod.TempPath)!.Length)..]}";
+                                        var path = GeneratePath(p, mod.ProjectName, cfg.IncludedPath);
+                                        Console.WriteLine(path);
+                                        DirectoryCopy(info.FullName, path, true);
+                                        //info.MoveTo(sb.ToString());
+                                    }
+                                }
+                            }
+                        }
+
+                        var fi = c.GetFiles().ToList();
+                        var ei = fi.Where(_ => _.Name.StartsWith("en_us", StringComparison.OrdinalIgnoreCase)).ToList();
+                        if (cfg.UpdateChinese) {
+                            ei.AddRange(fi.Where(_ => _.Name.StartsWith("zh_cn", StringComparison.OrdinalIgnoreCase)).ToList());
+                        }
+
+                        foreach (var info in ei) {
+                            if (info.Name.EndsWith(".lang")) {
+                                var p = $"{rootPath}{info.FullName[(info.FullName.LastIndexOf(Path.GetFileName(mod.TempPath)!, StringComparison.Ordinal) + Path.GetFileName(mod.TempPath)!.Length)..]}";
+                                var path = GeneratePath(p, mod.ProjectName, cfg.IncludedPath);
+                                //Console.WriteLine(path);
+                                Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+                                var lf = new LangFormatter(new StreamReader(File.OpenRead(info.FullName)),
+                                    new StreamWriter(File.Create(path)));
+                                lf.Format();
+                            }
+                            else if (info.Name.EndsWith(".json")) {
+                                var p = $"{rootPath}{info.FullName[(info.FullName.LastIndexOf(Path.GetFileName(mod.TempPath)!, StringComparison.Ordinal) + Path.GetFileName(mod.TempPath)!.Length)..]}";
+                                var path = GeneratePath(p, mod.ProjectName, cfg.IncludedPath);
+                                //Console.WriteLine(path);
+                                Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+                                var jf = new JsonFormatter(new StreamReader(File.OpenRead(info.FullName)),
+                                    new StreamWriter(File.Create(path)), mod.ProjectName);
+                                jf.Format();
+                            }
+                        }
                     }
-
-                    var root = directory.EnumerateDirectories()
-                        .Where(_ => include.Contains(_.Name))
-                        .Select(_ => _.FullName);
-
-                    root.ToList().ForEach(Console.WriteLine);
                 }
             }
+            //比较好看的
+            //foreach (var info in directories) {
+            //    foreach (var directory in info.GetDirectories()) {
+            //        if (directory.Name == "minecraft") {
+            //            continue;
+            //        }
+
+            //        var root = directory.EnumerateDirectories()
+            //            .Where(_ => include.Contains(_.Name))
+            //            .Select(_ => _.FullName);
+
+            //        root.ToList().ForEach(Console.WriteLine);
+            //    }
+            //}
+        }
+
+        /// <summary>
+        /// 白嫖的目录复制
+        /// </summary>
+        /// <param name="sourceDirName"></param>
+        /// <param name="destDirName"></param>
+        /// <param name="copySubDirs"></param>
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 往路径中插入项目名称
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="projectName"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        private static string GeneratePath(string path, string projectName,string[] root) {
+            var sb = new StringBuilder();
+            var p = path.Split("\\").ToList();
+            p.ForEach(_ =>
+            {
+                if (root.ToList().Contains(_))
+                {
+                    sb.Append(_ + "\\");
+                    sb.Append(projectName + "\\");
+                }
+                else if (_.EndsWith(".lang") || _.EndsWith(".json")) {
+                    sb.Append(_);
+                }
+                else
+                {
+                    sb.Append(_ + "\\");
+                }
+
+            });
+
+            return sb.ToString();
         }
     }
 }
--- src/Spider/Program.cs
+++ src/Spider/Program.cs
@@ -1,17 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text.Json;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Timers;
-using Formatter;
-using Language.Core;
 using Serilog;
 using Spider.Lib;
-using Spider.Lib.FileLib;
 using Spider.Lib.JsonLib;
 
 namespace Spider {
@@ -25,10 +17,11 @@ namespace Spider {
 
             var m = new Mod();
             m.TempPath = @"C:\Users\Nullpinter\Desktop\spacesniffer_1_3_0_2\thermal_foundation-1.16.3-1.1.6.jar";
+            m.ProjectName = "114514";
             var c = new Lib.JsonLib.Configuration();
             c.IncludedPath = new[] {"assets","data"};
-            c.ExtractPath = new[] {"lang", "patchouli_books" };
-            Utils.ParseFiles(m,c,"");
+            c.ExtractPath = new[] {"lang", "patchouli_books"};
+            Utils.ParseFiles(m,c, @"D:\repos\Minecraft-Mod-Language-Package\projects\1.12.2");
             Console.ReadLine();
         }
     }
