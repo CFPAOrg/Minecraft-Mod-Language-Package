c311d2e877d6cd45f1ed5da0ab2006abff164a7e
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2021-03-20 18:17:33 +0800

    [WIP]重构 (#924)
    * remove action
    
    * r-1
    
    * 重构的第一步
    
    * ru
    
    * u-p
    
    * update formatter
    
    * update
    
    * update
    
    * update
    
    * update
    
    * 完善新下载器
    
    * update
    
    * 爬
    
    * 噔噔咚
    
    * rua一下 *新整解析器
    
    * rua
    
    * rua
    
    * rua
    
    * rua*2
    
    * 进度：50%
    
    * 100%
    
    * 99%
    
    * 100%可能
    
    * 果然还差点
    
    * 101%
    
    * Revert "remove action"
    
    This reverts commit 2e1f33e49a899bd0b9246db862d21478caef8097.
    
    * Auto stash before revert of "remove action"
    
    * update act
    
    * fix
    
    * fix * 2
    
    * Update file
    
    * Update config.json
    
    * fix
    
    * fix
    
    * fix 2
    
    * fix 3
    
    * Create init
    
    * Update .gitignore
    
    * Update formatter.yml
    
    * Update formatter.yml
    
    * Update formatter.yml
    
    * fix 4
    
    * fix 14
    
    
    fix 6
    
    
    fix 7
    
    
    fix 8
    
    
    fix 9
    
    
    fix 10
    
    
    fix 11
    
    
    fix 12
    
    
    fix 13
    
    
    fix 14
    
    * fix 15
    
    * Update config.json
    
    * 可以试试这个，把多余的mod去掉
    
    * Update file
    
    * 试试能不能自动改（理论
    
    * Revert "Update file"
    
    This reverts commit e0cf06695c68eedaf1747d05afcb448c8a561b4d.
    
    * 加入狗屎的版本判断
    
    * Update file
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <actions@users.noreply.github.com>

---
+++ src/Spider/Lib/InfoParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Serilog;
+using Spider.Lib.JsonLib;
+
+namespace Spider.Lib {
+    public class InfoParser {
+        public List<ModInfo> Infos { get; set; }
+        private readonly Configuration _defaultConfiguration;
+        private readonly Configuration[] _customConfigurations;
+
+        public InfoParser(Configuration defaultConfiguration, Configuration[] customConfigurations) {
+            _defaultConfiguration = defaultConfiguration;
+            _customConfigurations = customConfigurations;
+        }
+
+        ~InfoParser() {
+            Log.Logger.Debug("解析器已回收");
+        }
+
+        public (ModInfo,Configuration)[] SerializeAll() {
+            if (Infos is null) {
+                throw new NullReferenceException("Empty infos!");
+            }
+
+            var tmp = new List<(ModInfo, Configuration)>();
+            foreach (var c in _customConfigurations) {
+                var mod = Infos.FirstOrDefault(_ => _.ShortWebsiteUrl == c.ProjectName);
+                Infos.Remove(mod);
+                if (mod is not null) {
+                    tmp.Add((mod, c));
+                }
+            }
+
+            foreach (var info in Infos) {
+                tmp.Add((info, _defaultConfiguration));
+            }
+
+            return tmp.ToArray();
+        }
+
+        public (ModInfo, Configuration) Serialize(ModInfo info) {
+            foreach (var cfg in _customConfigurations) {
+                if (cfg.ProjectName == info.ShortWebsiteUrl) {
+                    return (info, cfg);
+                }
+            }
+            return (info, _defaultConfiguration);
+        }
+    }
+}
