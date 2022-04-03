52f4a2a5bcb68062c88a208437274c8f02561f9b
TartaricAcid <baka943@qq.com>
2022-04-03 17:14:14 +0800

    修复脚本错误

--- src/Uploader/Program.cs
+++ src/Uploader/Program.cs
@@ -102,22 +102,22 @@ namespace Uploader
                         Log.Logger.Error(err2);
                         break;
                     case "1.16-fabric":
-                        var cmd2 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16-Fabric.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16-Fabric.zip");
-                        cmd2.Execute();
-                        var err2 = cmd2.Error;
-                        Log.Logger.Error(err2);
+                        var cmd3 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16-Fabric.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16-Fabric.zip");
+                        cmd3.Execute();
+                        var err3 = cmd3.Error;
+                        Log.Logger.Error(err3);
                         break;
                     case "1.18":
-                        var cmd3 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip");
-                        cmd3.Execute();
-                        var err3 = cmd3.Error;
-                        Log.Logger.Error(err3);
+                        var cmd4 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip");
+                        cmd4.Execute();
+                        var err4 = cmd4.Error;
+                        Log.Logger.Error(err4);
                         break;
                     case "1.18-fabric":
-                        var cmd3 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18-Fabric.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18-Fabric.zip");
-                        cmd3.Execute();
-                        var err3 = cmd3.Error;
-                        Log.Logger.Error(err3);
+                        var cmd5 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18-Fabric.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18-Fabric.zip");
+                        cmd5.Execute();
+                        var err5 = cmd5.Error;
+                        Log.Logger.Error(err5);
                         break;
                     default:
                         break;//不应该
