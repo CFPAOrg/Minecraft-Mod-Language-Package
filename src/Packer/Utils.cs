1f9283244d76a053cdb01bda9ac79402bac4fc3b
dovisutu <40313014+dovisutu@users.noreply.github.com>
2023-06-23 11:16:56 +0800

    Font Update - Phase 2
    Packer的字体部分全面更新，以及杂项更新
    
    - 将字符替换表更改为版本独立
      - 同时将配置文件也改为了版本独立，否则太大了
    - 修改了时间戳的加法：现在不会干掉原文件了
    - 杂项更新，如日志格式

--- src/Packer/Utils.cs
+++ src/Packer/Utils.cs
@@ -14,14 +14,16 @@ namespace Packer
 {
     static class Utils
     {
-        public static async Task<Config> RetrieveConfig(string configPath, string mappingPath, string version)
+        public static async Task<Config> RetrieveConfig(string configTemplate, string version)
         {
-            Log.Information("正在获取配置");
+            Log.Information("正在获取配置。目标版本：{0}", version);
+            
+            var configPath = string.Format(configTemplate, version);
+
+            Log.Information("配置位置：{0}", configPath);
+
             var reader = await File.ReadAllBytesAsync(configPath);
-            var configs = JsonSerializer.Deserialize<List<Config>>(reader);
-            var replacement = await ReadReplaceFontMap(mappingPath);
-            configs.ForEach(_ => _.CharatcerReplacement = replacement);
-            return configs.Where(_ => _.Version == version).FirstOrDefault(); // 仅选取指定版本，忽略重复
+            return JsonSerializer.Deserialize<Config>(reader);
         }
 
         public static PackerStrategy RetrieveStrategy(FileInfo file)
@@ -83,34 +85,19 @@ namespace Packer
             return mapping.Values;
         }
 
-        public static async Task<Dictionary<string, string>> ReadReplaceFontMap(string path) // 从隔壁弄过来改了一下，就放这里了
-        {
-            var mapping = new Dictionary<string, string>();
-            foreach (string str in await File.ReadAllLinesAsync(path))
-            {
-                var kv = str.Split('>', StringSplitOptions.TrimEntries);
-                var key = kv[0];
-                var value = kv[1];
-                Log.Verbose("添加了映射：{0} -> {1}", key, value);
-                mapping.Add(key, value);
-            }
-            return mapping;
-        }
-
         // 下面的这些...其实都不是我写的...
 
-        public static void CreateTimeStamp(string version)
+        public static string AppendTimestamp(string path)
         {
-            var mcmeta = $"./projects/{version}/pack.mcmeta";
+            var mcmeta = path;
             var meta = JsonSerializer.Deserialize<McMeta>(File.ReadAllText(mcmeta));
-            var time = DateTime.UtcNow.AddHours(8);
+            var time = DateTime.UtcNow.AddHours(8); // UTC+8:00
             meta.Pack.Description += $"\n打包时间：{time:yyyy-MM-ddTHH:mm:ssZ}";
-            var result = JsonSerializer.Serialize(meta, new JsonSerializerOptions()
+            return JsonSerializer.Serialize(meta, new JsonSerializerOptions()
             {
                 Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
                 WriteIndented = true
             });
-            File.WriteAllText(mcmeta, result);
         }
     }
 }
