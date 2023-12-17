3fc14cc31fd9ef8433b5c6d78152db7f0b2870f4
dovisutu <40313014+dovisutu@users.noreply.github.com>
2023-12-17 10:47:12 +0800

    Packer更新 / 1.20版本支持 (#3875)
    * Initialize 1.20
    
    * Fix paths
    
    * Miscellaneous Updates
    
    * Synchronize README format
    
    * Initial commit
    
    * 2
    
    * Finalize Packer code (ish)
    
    * Migration phase I
    
    * Migration Phase II
    
    * Post-work support
    
    支持部分文件打包
    （把测试的那一堆commit合进来了）
    
    * Modify behaviors on merging files
    
    * hotfix1
    
    * test1
    
    * test2
    
    * fix policies
    
    * Misc1
    
    * patch fail-on-cache-miss
    
    * Update Matrix Logic
    
    现在，（如果我没写错的话）一个版本崩溃不会炸掉所有版本了。
    此外，根据反馈，恢复了chinjufu里的大括号；不过打包文件里的括号可能留不下来了。
    
    * Fix Policies
    
    * Fix 2-EM Hyphen
    
    天，我怎么写成2e3f的...
    
    * Update projects/1.20/assets/more-crossbows-cj/morecrossbows/lang/zh_cn.json
    
    Co-authored-by: Cactus_student <97865546+Cactusstudent@users.noreply.github.com>
    
    * Update projects/1.20/assets/more-crossbows-cj/morecrossbows/lang/zh_cn.json
    
    Co-authored-by: Cactus_student <97865546+Cactusstudent@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Cactus_student <97865546+Cactusstudent@users.noreply.github.com>

---
+++ src/Packer/Models/Providers/RawFile.cs
@@ -0,0 +1,61 @@
+﻿using Packer.Extensions;
+using Serilog;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Packer.Models.Providers
+{
+    /// <summary>
+    /// 一般文件的提供器。不提供合并、替换支持
+    /// </summary>
+    /// <remarks>
+    /// 对于非文本文件，使用该类
+    /// </remarks>
+    public class RawFile : IResourceFileProvider
+    {
+        /// <summary>
+        /// 文件的源地址
+        /// </summary>
+        public FileInfo SourceFile { get; }
+
+        /// <inheritdoc/>
+        public string Destination { get; }
+
+        /// <summary>
+        /// 从给定的<see cref="FileInfo" />构造提供器
+        /// </summary>
+        /// <param name="sourceFile">源文件的引用</param>
+        /// <param name="destination">目标地址</param>
+        public RawFile(FileInfo sourceFile, string destination)
+        {
+            SourceFile = sourceFile;
+            Destination = destination;
+        }
+
+
+        /// <inheritdoc/>
+        public IResourceFileProvider ReplaceDestination(string searchPattern, string replacement)
+            => new RawFile(SourceFile,
+                           Regex.Replace(Destination,
+                                         searchPattern,
+                                         replacement,
+                                         RegexOptions.Singleline));
+
+        /// <inheritdoc/>
+        public async Task WriteToArchive(ZipArchive archive)
+        {
+            var destination = Destination.NormalizePath();
+            Log.Debug("[RawFile]写入路径 {0}", destination);
+
+            archive.ValidateEntryDistinctness(destination);
+
+            // 为什么这ZipArchive.CreateEntryFromFile没有Async变种...只有手动实现了
+            using var source = SourceFile.OpenRead();
+            using var entry = archive.CreateEntry(destination)
+                                     .Open();
+            await source.CopyToAsync(entry);
+        }
+    }
+}
